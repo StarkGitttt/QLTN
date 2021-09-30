45af1b16f804286bcdb93b89387169a61d4ee5e5
Your Name <you@example.com>
2021-09-30 11:29:55 +0700

    Add text

--- QuanLyNhaTro/src/gui/TestGit.java
+++ QuanLyNhaTro/src/gui/TestGit.java
@@ -12,11 +12,11 @@ package gui;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Quyen yeu Lam");
-<<<<<<< HEAD
-        System.out.println("Long nek");
-=======
+
+        System.out.println("Long nek1");
+
         System.out.println("Add text");
->>>>>>> b2dc1567e486bd1b31b2834e2b1c3d1c3cdef605
+
     }
             
 }
